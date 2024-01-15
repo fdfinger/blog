9e9403e54e04a421d2d1535dfb23c432ef8fd4d1
江飞 <jiangfei.ng@foxmail.com>
2024-01-15 15:33:36 +0800

    Update risk-value.js

--- source/js/components/risk/risk-value.js
+++ source/js/components/risk/risk-value.js
@@ -8,7 +8,7 @@ export default {
   props: ['title'],
   setup(props, ctx) {
     const form = ref({
-      amount: 1000, // 本金
+      amount: 1000, // 份额
       current: 1, // 当前价位
       stopLossPercentage: 2, // 止损
       risk: 0, // 风险
@@ -24,6 +24,11 @@ export default {
       return ((current * (100 - stopLossPercentage)) / 100).toFixed(2);
     })
 
+    const amount = /** 买入金额 */ computed(() => {
+      const { current, amount } = form.value;
+      return current * amount;
+    })
+
     return { form, risk, stopLoss };
   },
   template: `
@@ -34,7 +39,7 @@ export default {
           <v-form fast-fail @submit.prevent>
             <v-text-field
               v-model="form.amount"
-              label="本金"
+              label="份额"
               required
             ></v-text-field>
     
@@ -55,11 +60,14 @@ export default {
         </v-sheet>
       </v-col>
       <v-col md="6" cols="12">
-        <v-sheet height="150" class="mx-auto bg-surface-variant pa-2">
+        <v-sheet height="100" class="mx-auto bg-surface-variant pa-2">
           风险值: {{ risk }}
         </v-sheet>
-        <v-sheet height="150" class="mx-auto bg-warning pa-2">
+        <v-sheet height="100" class="mx-auto bg-warning pa-2">
           止损位: {{ stopLoss }}
+        </v-sheet>
+        <v-sheet height="100" class="mx-auto bg-success pa-2">
+          买入金额: {{ amount }}
         </v-sheet>
       </v-col>
     </v-row>
